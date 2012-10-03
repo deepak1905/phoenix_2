46a5224f135d3378b13a28ab8c67f5d0f9d72a55
Deepak Revanna <deepakr.1905@gmail.com>
2012-10-03 23:08:51 +0300

    initialization of ROM
    ROM initialization through file at the time of instantiation

--- VHDL/test_benches/single_port_ROM.vhd
+++ VHDL/test_benches/single_port_ROM.vhd
@@ -5,7 +5,7 @@
 -- File       : single_port_ROM.vhd
 -- Author     : Deepak Revanna  <revanna@pikkukeiju.cs.tut.fi>
 -- Company    : Tampere university of technology
--- Last update: 2012/09/05
+-- Last update: 2012/10/03
 -- Platform   : 
 -------------------------------------------------------------------------------
 -- Description: Single port ROM module enables single clock read
@@ -18,13 +18,15 @@
 
 --Include necessary library and
 --packages
-library ieee;
+library ieee, std;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
+use std.textio.all;
 
 entity single_port_ROM is
   
   generic (
+    FILE_NAME  : string := "rom_0.txt";
     ADDR_WIDTH : integer := 5;         -- the width of address bus configurable at the design time(default width for 64 point
                                        -- FFT  which requires storing 32 twiddle factors per bank)
     DATA_WIDTH : integer := 32);         
@@ -41,24 +43,46 @@ architecture rtl of single_port_ROM is
 
   subtype rom_word is std_logic_vector(DATA_WIDTH-1 downto 0);
   type ROM_memory is array ((2**ADDR_WIDTH)-1 downto 0) of rom_word;
-  signal ROM_init_value : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+  file file_handle : text;
 
   --function to initialize ROM with default values
-  function initialize_ROM
+  --NOTE: We cannot reference file parameters inside
+  --the pure function, hence the function is defined
+  --as an impure function
+  impure function initialize_ROM(file_name : string)
     return ROM_memory is
 
     --function to initialize ROM memory to
     --initial default values
     variable ROM_mem_init : ROM_memory;
+    file file_handle : text;
+    variable lineread : line;
+    variable dataread : integer;
+    variable is_open : boolean := false;
 
     begin
 
-      for i in 2**ADDR_WIDTH-1 downto 0 loop
-        
-        ROM_mem_init(i) := (others=>'0');
-        
+      if is_open = false then
+
+      file_open(file_handle, file_name, READ_MODE);
+      is_open := true;
+
+      end if;
+
+      --for 8 point FFT
+      for i in 0 to 3 loop
+
+        if (not endfile(file_handle)) then
+
+          readline(file_handle, lineread);
+          read(lineread, dataread);
+
+          ROM_mem_init(i) := conv_std_logic_vector(dataread, DATA_WIDTH);
+
+        end if;        
       end loop;  -- i
 
+      file_close(file_handle);
       return ROM_mem_init;
       
   end function initialize_ROM;
@@ -66,7 +90,7 @@ architecture rtl of single_port_ROM is
   --create ROM unit and initialize the contents with zeros
   --NOTE:ROM initialization to zeros can be done as (others =>(others=>'0'))
   --but used a function to initialize - just for a change.!
-  signal ROM_memory_bank : ROM_memory := initialize_ROM;
+  signal ROM_memory_bank : ROM_memory := initialize_ROM(FILE_NAME);
 
   begin
 
