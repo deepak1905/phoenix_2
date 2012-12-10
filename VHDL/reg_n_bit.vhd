fa38f847414f551571e6f25bbf27be2d0013a335
Deepak Revanna <deepakr.1905@gmail.com>
2012-12-10 21:00:56 +0200

    New scheme - working version

--- VHDL/reg_n_bit.vhd
+++ VHDL/reg_n_bit.vhd
@@ -1,12 +1,12 @@
 -------------------------------------------------------------------------------
 -- Title      : N bit register
--- Project    : 
+-- Project    : N point FFT processor
 -------------------------------------------------------------------------------
 -- File       : reg_n_bit.vhd
--- Author     : Deepak Revanna  <revanna@pikkukeiju.cs.tut.fi>
+-- Author     : Deepak Revanna  <deepak.revanna@tut.fi>
 -- Company    : Tampere University of Technology
--- Last update: 2012/10/17
--- Platform   : 
+-- Last update: 2012/12/05
+-- Platform   : Altera stratix II FPGA
 -------------------------------------------------------------------------------
 -- Description: Register with n-bit input
 -------------------------------------------------------------------------------
@@ -18,6 +18,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 entity reg_n_bit is
   
@@ -32,6 +34,9 @@ entity reg_n_bit is
     data_out : out std_logic_vector(DATA_WIDTH-1 downto 0));
 
 end reg_n_bit;
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 architecture rtl of reg_n_bit is
 
@@ -49,13 +54,20 @@ begin  -- rtl
       --set the output based on the load
       --control signal
       if rst = '0' then
-        reg <= (others => '0');
+        
+          reg <= (others => '0');
+          
       elsif clk'event and clk = '1' then
-        if load = '1' or load = 'H' then
-          reg <= data_in;
-        end if;
+        
+          if load = '1' or load = 'H' then
+            
+              reg <= data_in;
+              
+          end if;
+              
       end if;
-    end process REG_PROCESS;
+      
+  end process REG_PROCESS;
 
 end rtl;
 
